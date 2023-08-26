97f2ce0d33e45495b35604e116f951416aaf4fe6
Ngô Xuân Mạnh <manhngodh@gmail.com>
2023-08-26 11:51:18 +0000

    add sample for test

---
+++ people_202308261848.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.people (id, people_id, "role")
+VALUES
+(1, 'ed10da90453a261214ae44992d84e238b323d8921e0c1dfd16bfd6608a34697c', 1), -- manh-nx
+(2, '98c7bc915bd36e012b6ead54602a369163876a4c06964ea36cab3123a3f7c5c2', 1), --ha-pn
+(3, '78b3c21df8c7acf6e3d007f7ff99eef3d8d6aa1c5939582e55c1c4706b7250fc', 2), --duc-nx
+(4, '44b902185ecfa8f23794f3327b9b2a27380db0d2b83b466a060002d2a8c4ab67', 2) --huong-th
+;
